99c665ccebeeb902a556bbea12f9e7c5e71523b2
Vadim Vetrov <vetrovvd@gmail.com>
2023-12-26 19:55:07 +0000

    Hello, World!

--- api/src/main/kotlin/io/github/waujito/messenger/api/ApiApplication.kt
+++ api/src/main/kotlin/io/github/waujito/messenger/api/ApiApplication.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class ApiApplication{
 	@GetMapping("/")
-	fun hello(): String = "hello, worlds!"
+	fun hello(): String = "hello, world!"
 }
 
 fun main(args: Array<String>) {
