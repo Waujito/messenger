4a7b51cd9fe3dc07ced69a2a8254985b0ddf97e9
Vadim Vetrov <vetrovvd@gmail.com>
2024-01-01 22:34:17 +0000

    Added orders to repositories

--- api/src/main/kotlin/io/github/waujito/messenger/api/chat/ChatMembershipRepository.kt
+++ api/src/main/kotlin/io/github/waujito/messenger/api/chat/ChatMembershipRepository.kt
@@ -1,12 +1,13 @@
 package io.github.waujito.messenger.api.chat
 
 import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-interface ChatMembershipRepository : PagingAndSortingRepository<ChatMembership, UUID> {
+interface ChatMembershipRepository : PagingAndSortingRepository<ChatMembership, UUID>, CrudRepository<ChatMembership, UUID> {
     fun getById(id: UUID): ChatMembership
 
     fun findAllByUserId(userId: String, pageable: Pageable): List<ChatMembership>
--- api/src/main/kotlin/io/github/waujito/messenger/api/chat/messages/MessageRepository.kt
+++ api/src/main/kotlin/io/github/waujito/messenger/api/chat/messages/MessageRepository.kt
@@ -2,16 +2,16 @@ package io.github.waujito.messenger.api.chat.messages
 
 import io.github.waujito.messenger.api.chat.Chat
 import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.CrudRepository
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
-interface MessageRepository: PagingAndSortingRepository<Message, UUID> {
+interface MessageRepository: PagingAndSortingRepository<Message, UUID>, CrudRepository<Message, UUID> {
     fun getById(id: UUID): Message
 
-    fun findAllByChat(chat: Chat, pageable: Pageable): List<Message>
+    fun findAllByChatOrderByCreatedAtDesc(chat: Chat, pageable: Pageable): List<Message>
 
-    fun findAllByChatAndAuthorId(chat: Chat, authorId: String, pageable: Pageable): List<Message>
-//    fun findByChatPaged
+    fun findAllByChatAndAuthorIdOrderByCreatedAtDesc(chat: Chat, authorId: String, pageable: Pageable): List<Message>
 }
