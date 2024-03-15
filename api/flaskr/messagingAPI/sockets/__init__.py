eb822761ecc5ddd560546b49e60931070e8a865f
Vadim Vetrov <vetrovvd@gmail.com>
2024-03-15 20:03:34 +0300

    add cors support for socketio

--- api/flaskr/messagingAPI/sockets/__init__.py
+++ api/flaskr/messagingAPI/sockets/__init__.py
@@ -1,7 +1,7 @@
 from .chatSockets import MessagingNamespace
 from flask_socketio import SocketIO
 
-socketio = SocketIO()
+socketio = SocketIO(cors_allowed_origins="*")
 
 
 socketio.on_namespace(MessagingNamespace('/chat'))
